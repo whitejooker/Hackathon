6af66db8290030970f139cc7b212342eae240502
Qalib <qalib.sarayev@hotmail.com>
2016-03-12 16:37:58 +0400

    From Qalib

--- src/az/hackathon/database/helpers/UserHelper.java
+++ src/az/hackathon/database/helpers/UserHelper.java
@@ -1,8 +1,10 @@
 package az.hackathon.database.helpers;
 
+import az.hackathon.models.City;
 import az.hackathon.models.User;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -10,6 +12,20 @@ import java.sql.SQLException;
  */
 public class UserHelper extends Helper{
 
+
+
+
+
+    private static final String id = "id";
+    private static final String username = "username";
+    private static final String name = "name";
+    private static final String phone = "phone";
+    private static final String email = "email";
+    private static final String password = "password";
+    private static final String cityId = "city_id";
+    private static final String isBlocked = "is_blocked";
+    private static final String address = "address";
+    private static final String numberOfComplains = "number_of_complains";
 
 
     private static void setValuesIntoPreparedStatement(PreparedStatement statement, User user) throws SQLException{
@@ -20,7 +36,29 @@ public class UserHelper extends Helper{
         statement.setString(5, user.getPassword());
         statement.setInt(6, user.getCity().getId());
         statement.setBoolean(7, user.isBlocked());
-        //statement.set
-
+        statement.setString(8, user.getAddress());
+        statement.setInt(9, user.getNumberOfComplains());
     }
+
+    private User CreateUserFromResultSet(ResultSet resultSet) throws SQLException{
+        User user = new User();
+        City city = new City();
+        CityHelper ch = new CityHelper();
+        user.setId(resultSet.getInt(id));
+        user.setUsername(resultSet.getString(username));
+        user.setName(resultSet.getString(name));
+        user.setPhone(resultSet.getString(phone));
+        user.setEmail(resultSet.getString(email));
+        user.setPassword(resultSet.getString(password));
+        city = ch.getCity(resultSet.getInt(cityId));
+        user.setCity(city);
+        user.setBlocked(resultSet.getBoolean(isBlocked));
+        user.setAddress(resultSet.getString(address));
+        user.setNumberOfComplains(resultSet.getInt(numberOfComplains));
+        return user;
+    }
+
+
+
+
 }
