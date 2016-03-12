4475f50dd0191e67e32d4d1d384b5fa5204eaa46
Qalib <qalib.sarayev@hotmail.com>
2016-03-12 14:02:55 +0400

    From Qalib

---
+++ src/az/hackathon/models/City.java
@@ -0,0 +1,33 @@
+package az.hackathon.models;
+
+/**
+ * Created by Qalib on 3/12/2016.
+ */
+public class City {
+    private int id;
+    private String name;
+
+    public City() {
+    }
+
+    public City(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/az/hackathon/models/Meal.java
@@ -0,0 +1,19 @@
+package az.hackathon.models;
+
+import java.util.Date;
+
+/**
+ * Created by Qalib on 3/12/2016.
+ */
+public class Meal {
+    private int id;
+    private int state;
+    private String description;
+    private String name;
+    private int amount;
+    private Date date;
+    private String pictureExtension;
+    private boolean isActive;
+    private Type type;
+
+}
