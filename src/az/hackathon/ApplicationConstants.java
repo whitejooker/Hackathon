dc9ac39370873ad698bb1a3a3b5f38c8207cd679
whitejooker <l-fray@mail.ru>
2016-03-12 21:46:46 +0400

    First commit

--- src/az/hackathon/ApplicationConstants.java
+++ src/az/hackathon/ApplicationConstants.java
@@ -4,10 +4,11 @@ public interface ApplicationConstants{
 String JDBC_URI = "java:comp/env/jdbc/hackathon";
 
 String JSP_REGISTRATION = "register.jsp";
-String JSP_HOME_PAGE="home_page.jsp";
+String JSP_HOME_PAGE="homepage.jsp";
 
 String ATTR_MESSAGES = "messages";
 String ATTR_ALL_CITIES = "allCities";
 String ATTR_IS_LOGGED = "isLogged";
 String ATTR_USER = "userLogged";
+String ATTR_SUBMIT = "submit";
 }
