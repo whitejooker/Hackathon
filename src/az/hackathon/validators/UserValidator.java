175b4a5d4f86ecd42e90888cc4376be7425a6be9
whitejooker <l-fray@mail.ru>
2016-03-12 15:40:35 +0400

    First commit

--- src/az/hackathon/validators/UserValidator.java
+++ src/az/hackathon/validators/UserValidator.java
@@ -1,30 +1,38 @@
 package az.hackathon.validators;
 
-import az.hackathon.models.User;
 import az.hackathon.utils.RequestUtil;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserValidator{
-static final Map<String, Integer> MAX_LENGTH_OF_ATTRIBUTE = new HashMap<>( User.NUMBER_OF_ATTRIBUTES );
-static final Map<String, Integer> MIN_LENGTH_OF_ATTRIBUTE = new HashMap<>( User.NUMBER_OF_ATTRIBUTES );
+public static int MAX_USERNAME_LENGTH = 20;
+public static int MAX_PASSWORD_LENGTH = 20;
+public static int MIN_USERNAME_LENGTH = 5;
+public static int MIN_PASSWORD_LENGTH = 5;
 RequestUtil util;
-static {
-	MAX_LENGTH_OF_ATTRIBUTE.put( User.ATTR_USERNAME, 15 );
-	MIN_LENGTH_OF_ATTRIBUTE.put( User.ATTR_USERNAME, 5 );
-}
-UserValidator( HttpServletRequest request ){
+private List<String> errorMessages = new ArrayList<>( );
+
+private UserValidator( HttpServletRequest request ){
 	util = new RequestUtil( request );
 }
 
-public boolean checkAllFields( ){
-	return true;
+private boolean checkUsername( ){
+	final String username = util.getXSSsafeStringFromRequest( RequestUtil.PARAM_USERNAME ).toLowerCase( );
+	return !( username.length( ) < MIN_USERNAME_LENGTH || username.length( ) > MAX_USERNAME_LENGTH );
 }
 
-public boolean checkSingleField( String fieldName ){
-	final String value = util.getXSSsafeStringFromRequest( fieldName );
-	return true;
+private boolean checkPassword( ){
+	final String passwd = util.getXSSsafeStringFromRequest( RequestUtil.PARAM_PASSWORD );
+	final String confirm = util.getXSSsafeStringFromRequest( RequestUtil.PARAM_PASSWORD_CONFIRM );
+	return passwd.length( ) <= MAX_PASSWORD_LENGTH && passwd.length( ) >= MIN_PASSWORD_LENGTH && passwd.equals( confirm );
+}
+
+// getters and setters
+public boolean isValid( ){
+	boolean flag = true;
+	if( checkUsername( ) ) errorMessages.add( "Username must be from 5-20 A-z0-9 symbols." );
+	return flag;
 }
 }
