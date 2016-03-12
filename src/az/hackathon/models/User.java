93cd45e4b4c898c9d4ee570f9633ac4e264c027e
Qalib <qalib.sarayev@hotmail.com>
2016-03-12 15:10:43 +0400

    From Qalib

--- src/az/hackathon/models/User.java
+++ src/az/hackathon/models/User.java
@@ -1,19 +1,116 @@
 package az.hackathon.models;
 
-import java.util.HashMap;
-import java.util.Map;
+/**
+ * Created by Qalib on 3/12/2016.
+ */
 
 public class User{
-public static final int NUMBER_OF_ATTRIBUTES = 6;
-public static final String ATTR_USERNAME = "username";
-public static final String ATTR_NAME = "name";
-public static final String ATTR_PHONE = "phone";
-public static final String ATTR_EMAIL = "email";
-public static final String ATTR_PASSWORD = "password";
-public static final String ATTR_ADDRESS = "address";
+    private int id;
+    private String username;
+    private String name;
+    private String phone;
+    private String email;
+    private String password;
+    private City city;
+    private boolean isBlocked;
+    private String address;
+    private int numberOfComplains;
 
-Map<String, String> attributes=new HashMap<>(  );
 
-public User( ){
-}
+    public User(){
+
+    }
+
+    public User(int id, String username, String name, String phone, String email, String password, City city, boolean isBlocked, String address, int numberOfComplains) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        this.city = city;
+        this.isBlocked = isBlocked;
+        this.address = address;
+        this.numberOfComplains = numberOfComplains;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public boolean isBlocked() {
+        return isBlocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        isBlocked = blocked;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getNumberOfComplains() {
+        return numberOfComplains;
+    }
+
+    public void setNumberOfComplains(int numberOfComplains) {
+        this.numberOfComplains = numberOfComplains;
+    }
 }
