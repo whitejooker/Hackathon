e6407c36fc7ae6b9fe57c9ee250379c0fff90923
Qalib <qalib.sarayev@hotmail.com>
2016-03-12 14:12:04 +0400

    From Qalib

--- src/az/hackathon/models/City.java
+++ src/az/hackathon/models/City.java
@@ -30,4 +30,12 @@ public class City {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
